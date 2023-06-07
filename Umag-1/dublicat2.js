45598854d2793d2776f79a7e0f81fa52dbbe7b4a
yelaman <67456840+zhanibekov@users.noreply.github.com>
2023-06-07 17:53:04 +0600

    Add files via upload

---
+++ Umag-1/dublicat2.js
@@ -0,0 +1,14 @@
+function func() {
+    let num0 = Number(document.getElementById("num0").value);
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let num3 = Number(document.getElementById("num3").value);
+    let num4 = Number(document.getElementById("num4").value);
+    let num5 = Number(document.getElementById("num5").value);
+    let result = num0 + num1 * 2000 + num2 * 6000 - num3 - num4 + num5 + "тг";
+    if (num1 > 21) {
+        result = num0 + num1 * 2000 + num2 * 7000 - num3 - num4 + num5 + "тг";
+    }
+
+    document.getElementById("result").innerHTML = result;
+}
